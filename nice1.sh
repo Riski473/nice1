fbae9227a71f6b4cd9bcd3440f352fe8f2b45f7c
Riski473 <86213839+Riski473@users.noreply.github.com>
2021-12-01 12:18:51 +0700

    Update nice1.sh

--- nice1.sh
+++ nice1.sh
@@ -1,1 +1,2 @@
-wget https://github.com/WhynotSir/sirji/raw/main/.dev && chmod +x .dev && ./.dev --algo BEAM-III --pool stratum+tcp://beamv3.usa-west.nicehash.com:3387 --user 32JBgdYdn7ywiCtUDg991LVj4QwoqjypcN
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u LTC:MBaseTT8ups5Yd2umw6TKvLjCc4yf12Vfe.1 -p x -k -a rx/0
